840021bef741f489ce72f51880772afaca255159
tdhuynh <tommyhuynh93@gmail.com>
2017-03-25 12:40:24 -0400

    added login html and profile detail

--- tour_tracker/main/views.py
+++ tour_tracker/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from main.models import Profile
+from django.views.generic import TemplateView, DetailView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm, User
 from django.urls import reverse_lazy, reverse
@@ -13,3 +14,7 @@ class UserCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
+
+
+class ProfileDetailView(DetailView):
+    model = Profile
--- tour_tracker/tour_tracker/urls.py
+++ tour_tracker/tour_tracker/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from main.views import IndexView, UserCreateView
+from main.views import IndexView, UserCreateView, ProfileDetailView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^$', IndexView.as_view(), name='index_view'),
     url(r'^new_user/$', UserCreateView.as_view(), name='user_create_view'),
+    url(r'^accounts/profile/(?P<pk>\d+)/$', ProfileDetailView.as_view(), name='profile_detail_view'),
 
 ]
