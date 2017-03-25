81aab7e6dd0795b11cad483fe85ec92de27e0be8
tdhuynh <tommyhuynh93@gmail.com>
2017-03-25 12:25:17 -0400

    added basic navbar links

--- tour_tracker/tour_tracker/urls.py
+++ tour_tracker/tour_tracker/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from main.views import IndexView, UserCreateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('django.contrib.auth.urls')),
     url(r'^$', IndexView.as_view(), name='index_view'),
     url(r'^new_user/$', UserCreateView.as_view(), name='user_create_view'),
-    
+
 ]
