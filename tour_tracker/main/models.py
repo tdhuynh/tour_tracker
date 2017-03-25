e3d2b3d4a133d24010bff8aa55091eeb61db49ce
tdhuynh <tommyhuynh93@gmail.com>
2017-03-25 11:36:33 -0400

    created receiver for profile creation

--- tour_tracker/main/models.py
+++ tour_tracker/main/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+
+@receiver(post_save, sender='auth.User')
+def create_user_profile(**kwargs):
+    created = kwargs.get('created')
+    instance = kwargs.get('instance')
+    if created:
+        Profile.objects.create(user=instance)
 
 
 class Profile(models.Model):
