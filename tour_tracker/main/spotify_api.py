9dfc7bf0f552f22feee89684e709b8e52b988be4
tdhuynh <tommyhuynh93@gmail.com>
2017-03-27 11:52:06 -0400

    working on api

--- tour_tracker/main/spotify_api.py
+++ tour_tracker/main/spotify_api.py
@@ -1,6 +1,7 @@
 import requests
+import json
 
-result = requests.get("https://api.spotify.com/v1/artists/{id}")
-print(result)
+result = requests.get("https://api.spotify.com/v1/artists/0OdUWJ0sBjDrqHygGUXeCF")
 json_result = result.json()
-print(json_result)
+pretty_result = json.loads(json_result)
+print(json.dumps(pretty_result, indent=4, sort_keys=True))
