0cf10975d1da11999277db0ba9cf07600e5a8192
tdhuynh <tommyhuynh93@gmail.com>
2017-03-27 15:48:39 -0400

    testing api

--- tour_tracker/main/spotify_api.py
+++ tour_tracker/main/spotify_api.py
@@ -1,7 +1,10 @@
 import requests
-import json
 
+name = input('band name: ').lower()
 result = requests.get("https://api.spotify.com/v1/artists/0OdUWJ0sBjDrqHygGUXeCF")
 json_result = result.json()
-pretty_result = json.loads(json_result)
-print(json.dumps(pretty_result, indent=4, sort_keys=True))
+
+if name == json_result['name'].lower():
+    print(json_result['name'])
+else:
+    print('artist does not exist')
